b86fabe25b97a0063220249e36154a4e3b50977d
nikkig <nikkig@nikkigs-MacBook-Pro.local>
2022-12-13 16:07:56 -0800

    search products functionality and link to details

--- src/components/SearchProducts/SearchProducts.jsx
+++ src/components/SearchProducts/SearchProducts.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 // styles
 import './SearchProducts.css'
 
-export default function SearchProducts() {
+export default function SearchProducts({ products }) {
+  const [searchInput, setSearchInput] = useState("")
+
+  //filter product names 
+  const filteredProducts = products.filter(p => p.name.includes(searchInput))
+  //map list of filtered products with product name as link to details
   return (
     <div className="search-products">
-      <input type="text" placeholder="Search Products" />
+      <input type="text" 
+             placeholder="Search Products"
+             value={searchInput}
+             onChange={(e) => setSearchInput(e.target.value)} />
       <ul>
-        <li>search results mapped here</li>
+        {searchInput !== "" && filteredProducts.map(p => {
+          return (
+            
+              <li key={p.id}><Link to={`/${p.id}`}>{p.name}</Link></li>
+           
+          )
+        })}
       </ul>
     </div>
   )
